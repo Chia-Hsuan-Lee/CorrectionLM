b5979a94dfffc355c443370211a6b5ddb86bd31a
Chia-Hsuan Lee <chiahlee@g3063.hyak.local>
2024-10-30 16:18:01 -0700

    push MWOZ code

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# CorrectionLM
-Source code for CorrectionLM: Self-Corrections with SLM for Dialogue State Tracking
+# CorrectionLM: Self-Corrections with SLM for Dialogue State Tracking
+This is the original implementation of "[CorrectionLM: Self-Corrections with SLM for Dialogue State Tracking](https://arxiv.org/abs/2410.18209)" by [Chia-Hsuan Lee](https://chiahsuanlee.github.io/), [Hao Cheng](https://sites.google.com/site/hcheng2site) and [Mari Ostendorf](https://people.ece.uw.edu/ostendorf/).
+
+<p align="center">
+  <img src="correctionlm.pdf" width="60%" height="60%">
+</p>
+
+The task is to track user intents predefined by a schema (ontology) in a multi-turn conversation with an agent. 
+CorrectionLM is a novel correction framework that enables Small Language Models (e.g. Llama3-8B) to self-correct using in-context exemplars without LLM (e.g. GPT-4o) involvement. 
+
+[**Installation**](#Installation) | [**Preprocess**](#Download-and-Preprocess-Data) | [**Training**](#Training) | [**Inference**](#Inference) | | [**Evaluation**](#Evaluation) | | [**Citation**](#Citation-and-Contact)
+
+## Installation #TODO
+
+Create a conda environment
+```console
+conda env create -f env.yml 
+```
+
+## Download and Preprocess Data
+
+To download and create the [MultiWoz 2.4](https://github.com/smartyfh/MultiWOZ2.4/) 
+```console
+cd data
+python create_data.py --main_dir mw24_src --mwz_ver 2.4 --target_path mw24
+python sample.py --input_fn mw24/train_dials.json --target_fn mw21_5p_train.json --ratio 0.05 --seed 1
+```
+
+
+## Training
+The first step of the training is to obtain SLM predictions using ICL in order to provide supervision signals for the correction training.
+```console
+python run_mwoz_ICL_5shot.py \
+      --output_dir expts/llama3_on_train5p_zeroshot/  \
+      --lm meta-llama/Meta-Llama-3-8B-Instruct \
+      --test_fn data/mw21_5p_train.json \
+      --mwz_ver 2.4
+```
+You can also use GPT-4o for comparisons
+```console
+python run_mwoz_ICL_5shot.py \
+      --output_dir expts/gpt4o_on_train5p_zeroshot/  \
+      --lm gpt4 \
+      --test_fn data/mw21_5p_train.json \
+      --mwz_ver 2.4
+```
+
+Then we create the in-context exemplars to finetune the SLM. Unlike traditional ICL methods that only consider the input and gold output, we also incoporate the model’s (erroneous) self predictions.
+```console
+python create_mwoz_llama_SFT_prompt.py \
+      --train_fn expts/llama3_on_train5p_zeroshot/running_log.json \
+      --retriever_dir retriever/expts/mw21_5p/ \
+      --output_fn data/llama3_on_train5p_zeroshot_ICL_prompt.json  \
+      --test_fn expts/llama3_on_train5p_zeroshot/running_log.json \
+      --mwz_ver 2.4
+```
+
+The second step is to train the SLM. In order to be computation-efficient, we aadopt [QLoRA](https://arxiv.org/abs/2305.14314) for training, i.e. we quantize the SLM then insert LoRA adapaters.
+```console
+sh train.sh
+```
+
+
+## Inference
+The first step of the inference is to get initial predictions by a non-finetuned SLM.
+```console
+python run_ICL_vanilla.py \
+      --train_fn data/mw21_5p_train.json \
+      --retriever_dir retriever/expts/mw21_5p/ \
+      --lm meta-llama/Meta-Llama-3-8B-Instruct \
+      --output_dir expts/llama3_train_5p_on_test100p/  \
+      --test_fn data/mw24_100p_test.json \
+      --mwz_ver 2.4 
+```
+
+We then prompt the correction-tuned SLM (correction SLM) to refine the initial predictions made in the first step.
+```console
+python run_correctionlm.py \
+    --train_fn expts/llama3_on_train5p_zeroshot/running_log.json \
+    --retriever_dir retriever/expts/mw21_5p/ \
+    --output_dir expts/correction_outputs/llama_example_llama_inference_train5p_test100p/  \
+    --test_fn expts/llama3_train_5p_on_test100p/running_log.json \
+    --mwz_ver 2.4 \
+    --model expts/correction_models/sft_llama3_on_train5p_zeroshot/
+done
+```
+
+## Evaluation
+Compute the JGA and F1 for both dialogue level (DST) and turn level (TLB). 
+```console
+python eval_result.py \
+      --eval_fn expts/correction_outputs/llama_example_llama_inference_train5p_test100p/running_log.json \
+      --eval_mode second_pass # first_pass to score on the results produced by non-finetuned LM
+```
+
+
+## Citation and Contact
+
+If you find our code or paper useful, please cite the paper:
+```bib
+```
+
+Please contact Chia-Hsuan Lee (chiahsuan.li[at]gmail.com) for questions and suggestions.
---
+++ utils/sql_sgd.py
@@ -0,0 +1,132 @@
+import sqlparse
+from collections import OrderedDict
+
+
+def sql_pred_parse(pred):
+    # parse sql results and fix general errors
+
+    #TODO: already added the space!
+    pred = " * FROM " + pred
+    # fix for no states
+    if pred == " * FROM  WHERE ":
+        return {}
+
+    # Here we need to write a parser to convert back to dialogue state
+    pred_slot_values = []
+    # pred = pred.lower()
+    parsed = sqlparse.parse(pred)
+    if not parsed:
+        return {}
+    stmt = parsed[0]
+    #print("stmt", stmt)
+    sql_toks = pred.split()
+    operators = [" = ", " LIKE ", " < ", " > ", " >= ", " <= "]
+
+    if " AS " in pred:
+        as_indices = [i for i, x in enumerate(sql_toks) if x == "AS"]
+
+        table_name_map_dict = {}
+        for indice in as_indices:
+            table_name_map_dict[sql_toks[indice + 1].replace(",", "")] = sql_toks[indice - 1]
+
+        #slot_values_str = str(stmt.tokens[-1]).replace("_", " ").replace("""'""", "").replace("WHERE ", "")
+        slot_values_str = str(stmt.tokens[-1]).replace("""'""", "").replace("WHERE ", "")
+        for operator in operators:
+            slot_values_str = slot_values_str.replace(operator, "-")
+        slot_values = slot_values_str.split(" AND ")
+
+        for sv in slot_values:
+            for t_ in table_name_map_dict.keys():
+                sv = sv.replace(t_ + ".", table_name_map_dict[t_] + "-")
+            pred_slot_values.append(sv)
+    else:
+
+        table_name = sql_toks[sql_toks.index("FROM") + 1]
+        slot_values_str = str(stmt.tokens[-1]).replace("""'""", "").replace("WHERE ", "")
+        for operator in operators:
+            slot_values_str = slot_values_str.replace(operator, "-")
+        slot_values = slot_values_str.split(" AND ")
+        pred_slot_values.extend([table_name + "-" + sv for sv in slot_values if slot_values != ['']])
+
+    pred_slot_values = {'-'.join(sv_pair.split('-')[:-1]): sv_pair.split('-')[-1] for sv_pair in pred_slot_values}
+
+    # remove _ in SQL columns # don't need it in sgd
+    #pred_slot_values = {slot.replace('_', ' '): value for slot, value in pred_slot_values.items()}
+
+    # fix typos
+    # pred_slot_values, _ = typo_fix(pred_slot_values)
+
+    #TODO: already removed ";" from last value
+    for slot, value in pred_slot_values.items():
+        if value[-1] == ";":
+            pred_slot_values[slot] = value[:-1]
+
+    return { slot: value for slot, value in pred_slot_values.items() if "-" in slot and value}
+
+
+def sv_dict_to_string(svs, sep=' ', sort=True):
+    result_list = [f"{s.replace('-', sep)}{sep}{v}" for s, v in svs.items()]
+    if sort:
+        result_list = sorted(result_list)
+    return ', '.join(result_list)
+
+
+def slot_values_to_seq_sql(original_slot_values, single_answer=False):
+    sql_str = ""
+    tables = OrderedDict()
+    col_value = dict()
+
+    # add '_' in SQL columns
+    slot_values = {}
+    for slot, value in original_slot_values.items():
+        if ' ' in slot:
+            slot = slot.replace(' ', '_')
+        slot_values[slot] = value
+
+    for slot, value in slot_values.items():
+        assert len(slot.split("-")) == 2
+
+        if '|' in value:
+            value = value.split('|')[0]
+
+        table, col = slot.split("-")  # slot -> table-col
+
+        if table not in tables.keys():
+            tables[table] = []
+        tables[table].append(col)
+
+        # sometimes the answer is ambiguous
+        if single_answer:
+            value = value.split('|')[0]
+        col_value[slot] = value
+
+    # When there is only one table
+    if len(tables.keys()) == 1:
+        where_clause = []
+        table = list(tables.keys())[0]
+        for col in tables[table]:
+            where_clause.append("{} = {}".format(col, col_value["{}-{}".format(table, col)]))
+        sql_str = "SELECT * FROM {} WHERE {}".format(table, " AND ".join(where_clause))
+    # When there are more than one table
+    else:
+        # We observed that Codex has variety in the table short names, here we just use a simple version.
+        from_clause = []
+        where_clause = []
+        for i, table in enumerate(tables.keys()):
+            t_i = "t{}".format(i + 1)
+            from_clause.append("{} AS {}".format(table, t_i))
+            for col in tables[table]:
+                where_clause.append("{}.{} = {}".format(t_i, col, col_value["{}-{}".format(table, col)]))
+        sql_str = "SELECT * FROM {} WHERE {}".format(", ".join(from_clause), " AND ".join(where_clause))
+
+    return sql_str
+
+
+#pred = "RideSharing_2 WHERE ride_type = 'Regular' AND number_of_seats = 4 ORDER BY ride_fare ASC LIMIT 1"
+#pred = "RideSharing_2 WHERE destination = 1008 Blossom Hill Road"
+#pred = "Travel_1 WHERE good_for_kids = True"
+#pred = "Calendar_1 AS t1, Services_1 AS t2 WHERE t1.event_date = March 4th AND t2.appointment_date = March 4th AND t2.appointment_time = half past 1 in the afternoon"
+pred = "RideSharing_2 WHERE number_of_seats = 4 ORDER BY ride_fare ASC LIMIT 1"
+pred_slot_values = sql_pred_parse(pred)
+
+#print(pred_slot_values)
